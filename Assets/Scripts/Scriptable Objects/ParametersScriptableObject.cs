9dd100b2680f4098afd5fb3205f20210461b86d8
o503@interia.eu <o503@interia.eu>
2022-11-13 12:41:39 +0100

    Added Parameters Scriptable Object

---
+++ Assets/Scripts/Scriptable Objects/ParametersScriptableObject.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "ParametersScriptableObject")]
+public class ParametersScriptableObject : ScriptableObject
+{
+    public float _playerSpeed;
+    public float _playerDamage;
+    public float _drillSpeed;
+    public float _o2Capacity;
+}

74335bf46f998c166e7a66d6119df99331cc6f8d
o503@interia.eu <o503@interia.eu>
2022-11-13 13:45:48 +0100

    Prototype of shop UI

---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+
+
+public class ShopManager : MonoBehaviour
+{
+    [SerializeField] private ParametersScriptableObject _playerParameters;
+    [SerializeField] private TextMeshProUGUI _speedDisplay;
+    [SerializeField] private TextMeshProUGUI _damageDisplay;
+    private int _speedMultiplier = 1;
+    private int _damageMultiplier = 1;
+
+    private void Awake() => UpdateParameters();
+
+    public void UpgradeSpeed()
+    {
+        ++_speedMultiplier;
+        UpdateParameters();
+    }
+
+    public void UpgradeDamage()
+    {
+        ++_damageMultiplier;
+        UpdateParameters();
+    }
+
+    private void UpdateParameters()
+    {
+        // Update values in scriptable object
+        _playerParameters._playerSpeed = _speedMultiplier * .2f;
+        _playerParameters._playerDamage = _damageMultiplier * 10f;
+
+        // Update values displayed in shop
+        _speedDisplay.text = string.Format("{0:N1}", _playerParameters._playerSpeed);
+        _damageDisplay.text = string.Format("{0:N1}", _playerParameters._playerDamage);
+    }
+}

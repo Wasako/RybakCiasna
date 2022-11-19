89a110f8bf51a539d342a78dd40a8fa770136a3a
rickimanda <a.krawczuk1@gmail.com>
2022-11-19 22:04:06 +0100

    improved terrain generation script & more (look desc)
    made player into prefab and put it on terrain_testing scene. added colliders to player prefab and tilemap with terrain. made a custom inspector window with buttons to generate/ clear terrain. added gizmo to show size of terrain to generate. added comments to code.

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -5,64 +5,97 @@ using UnityEngine.Tilemaps;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    [SerializeField] int width = 1, height = 1, fillBorder = 50;
+    // fields that are relevant to config of generation 
+    [Header("Tilemap options")]
     [SerializeField] Tilemap terrainTilemap;
-    [SerializeField] Tile filled, empty;
+    [SerializeField] Tile filled;
 
-    [Range(0,100)]
-    [SerializeField] float smoothness = 20f;
-    float seed;
+    [Header("Size of generated map")]
+    [Range(0, 1000)][SerializeField] int width = 1;
+    [Range(0, 1000)][SerializeField] int height = 1;
 
-    int[][] generationResult;
+    [Header("Generation options")]
+    [Tooltip("small - big chunks")][SerializeField] float smoothness = 20f;
+    [Tooltip("small - more filled")][SerializeField] int fillBorder = 50;
+
+    float seed; // should not be visible or changeable
 
     void Start()
     {
-        seed = Random.Range(-1000000, 1000000);
-        Generation();
+        // generate terrain, if none is generated yet
+        if (!terrainTilemap.ContainsTile(filled)) {
+            seed = Random.Range(-1000000, 1000000);
+            GenerateTerrain();
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        // keys to quickly generate, clear terrain
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            seed = Random.Range(-1000000, 1000000);
-            Generation();
+            ButtonNewTerrain();
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.O))
         {
-            Generation();
+            ButtonSameTerrain();
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            terrainTilemap.ClearAllTiles();
+            ButtonClearTerrain();
         }
     }
+
+    private void OnDrawGizmos() {
+        // displays a gizmo of the size and position of terrain that is/ will be generated
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(new Vector3(width/2, height/-2, 0), new Vector3(width, height, 0));
+    }
     
-    void Generation()
+    public void GenerateTerrain()
     {
+        // iterates through all tiles in defined area
         for (int x = 0; x < width; x++)
         {
-            for (int y = 0; y < height; y++)//This will help spawn a tile on the y axis
+            for (int y = height*-1; y < 0; y++)
             {
+                // calls the perlin noise function, and if output is larger than some threshold,
+                // fills in the tile
                 if (Perlin(x, y) > fillBorder)
                 {
                     terrainTilemap.SetTile(new Vector3Int(x, y, 0), filled);
                 }
             }
-
         }
     }
 
+    // function that calculates an output from Perlin noise 
     int Perlin(int xCoord, int yCoord) {
         
         float x = (float)xCoord / width * smoothness + seed;
-        float y = (float)yCoord / width * smoothness + seed;
+        float y = (float)yCoord / height * smoothness + seed;
 
         int result = Mathf.RoundToInt (Mathf.PerlinNoise(x, y) * 100);
-        // Debug.Log(result);
         return result;
     }
 
-    //todo: rename parameters, add comments
+    // functions that are called when buttons are pressed
+    // both on keyboard (I, O, P) and in inspector
+    public void ButtonClearTerrain()
+    {
+        terrainTilemap.ClearAllTiles();
+    }
 
+    public void ButtonNewTerrain()
+    {
+        terrainTilemap.ClearAllTiles();
+        seed = Random.Range(-1000000, 1000000);
+        GenerateTerrain();
+    }
+
+    public void ButtonSameTerrain()
+    {
+        terrainTilemap.ClearAllTiles();
+        GenerateTerrain();
+    }
 }

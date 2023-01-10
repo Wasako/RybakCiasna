b6355b5b279ac89c92f21f214f2b6c79d78db588
Adam Madela <dungald@gmail.com>
2023-01-10 01:01:27 +0100

    enemy ai

--- Assets/Scripts/Enemies/LeftAndRightEnemyAI.cs
+++ Assets/Scripts/Enemies/LeftAndRightEnemyAI.cs
@@ -1,19 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class UpAndDownEnemyAI : BaseEnemyAi
+public class LeftAndRightEnemyAI : BaseEnemyAi
 {
     [SerializeField]
     [Range(0f, 10f)]
-    [Tooltip("How far will the AI travel verticaly")]
+    [Tooltip("How far will the AI travel horizontaly")]
     protected float travelDistance;
-    
+
     private Vector3 startingPosition;
 
     new void Start()
-    {    
+    {
         startingPosition = transform.position;
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
@@ -23,11 +22,10 @@ public class UpAndDownEnemyAI : BaseEnemyAi
     {
         float step = movementSpeed * Time.deltaTime;
 
-        if (startingPosition.x == 0f)
+        if (startingPosition.y == 0f)
         {
 
         }
-        
-        rb.MovePosition(startingPosition + transform.up * Mathf.Sin(Time.time) * travelDistance);
+        rb.MovePosition(startingPosition + transform.right * Mathf.Sin(Time.time) * travelDistance);
     }
 }
